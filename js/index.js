9fe95739e8da98aba664e2b91a63d4dd05f52966
Zhangzhenhuazhenhua <471917496@qq.com>
2019-06-01 15:37:06 +0800

    123

---
+++ js/index.js
@@ -0,0 +1,82 @@
+window.onload=function (){
+    let tablists = document.querySelectorAll('.diary-head>li')
+    tablists.forEach(function (elem,index) {
+        elem.onclick=function () {
+            for (let i=0;i<tablists.length;i++) {
+                tablists[i].classList.remove('yi');
+            }
+            this.classList.add('yi')
+        }
+
+    })
+
+    // let index=0;
+    // let rightBtn=document.querySelector('.rightBtn');
+    // let bannerImg=document.querySelectorAll('.lr-button > li')
+    // rightBtn.onclick=function(){
+    //     index++;
+    //     bannerImg.forEach(function (els) {
+    //         els.style.zIndex= 1;
+    //
+    //     });
+    //     bannerImg[index].style.zIndex= 999;
+    // }
+
+
+    let tablist = document.querySelectorAll('.tab-text >li')
+    tablist.forEach(function (elem,index) {
+        elem.onmouseenter=function () {
+            for (let i=0;i<tablist.length;i++) {
+                tablist[i].classList.remove('hot');
+            }
+            this.classList.add('hot')
+        }
+
+    })
+
+
+    let current=0, next=0;
+    let rightBtn=document.querySelector('.right-button');
+    let leftBtn=document.querySelector('.left-button');
+    let bannerImg=document.querySelectorAll('.lr-button>li');
+    let w=bannerImg[0].offsetWidth;
+    rightBtn.onclick=function () {
+        next++;
+
+        bannerImg[next].style.left=w+'px'
+        animate(bannerImg[current],{left:-w});
+        animate(bannerImg[next],{left: 0});
+        current=next;
+
+    }
+    leftBtn.onclick=function () {
+        next++;
+
+        animate(bannerImg[current],{left:-w});
+        animate(bannerImg[next],{left: 0});
+        current=next;
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
